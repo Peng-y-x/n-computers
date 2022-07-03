af6294e4fdbce5c1308c7700b20522329eec78b1
Pengyx <pengyxyx@atlassian.com>
2022-07-03 11:46:03 +0800

    软件技术进展讲座大作业：同时运行N台电脑的最长时间 2.0

--- src/NComputers.java
+++ src/NComputers.java
@@ -1,6 +1,32 @@
 public class NComputers {
     static int n;//��������
     static int []batteries;//�������
+
+    static long solution() {
+        n = 2;
+        batteries = new int[]{1, 1, 1, 1};
+
+        int m = batteries.length;
+        if(m < n) {
+            return 0;
+        }
+        long left = 0;
+        long right = batteries[0];
+        long ans = 1;
+        for(int num : batteries) {
+            left = Math.min(left, num);
+            right += num;
+        }
+        while(left <= right) {
+            long mid = (left + right) >> 1;
+            if(check(n, batteries, mid)) {
+                ans = mid;
+                left = mid + 1;
+            }
+            else right = mid - 1;
+        }
+        return ans;
+    }
     static boolean check(int n, int[] batteries, long t) {
         long sum = 0;
         for(int num : batteries) {
@@ -10,31 +36,6 @@ public class NComputers {
     }
 
     static public void main(String[] args) {
-        n = 2;
-        batteries = new int[]{1, 1, 1, 1};
-
-        int m = batteries.length;
-        if(m < n) {
-            System.out.println("电池数过少，运行时间为0.");
-        }
-        else{
-            long left = 0;
-            long right = batteries[0];
-            long ans = 1;
-            for(int num : batteries) {
-                left = Math.min(left, num);
-                right += num;
-            }
-            while(left <= right) {
-                long mid = (left + right) >> 1;
-                if(check(n, batteries, mid)) {
-                    ans = mid;
-                    left = mid + 1;
-                }
-                else right = mid - 1;
-            }
-            System.out.println("运行时间为"+ans);
-        }
-
+        System.out.println(solution());
     }
 }
